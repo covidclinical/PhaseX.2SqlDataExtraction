8ad3ac9b58b4266851bbd606a7fbb417fc172825
Griffin Weber <weber@hms.harvard.edu>
2022-08-14 22:35:37 -0400

    Add files via upload

--- Extras/4CE_PhaseX.2_CustomCohorts_MISCPatients_mssql.sql
+++ Extras/4CE_PhaseX.2_CustomCohorts_MISCPatients_mssql.sql
@@ -1,28 +1,26 @@
 --------------------------------------------------------------------------------
--- Custom cohorts: All other MISC patients (OtherMISCAdm and OtherMISCNotAdm)
+-- Custom cohorts: All MISC patients
 -- Database: Microsoft SQL Server
 -- Data Model: i2b2
 -- Created By: Griffin Weber (weber@hms.harvard.edu)
 --
--- This code creates two additional cohorts capturing all patients who are not
--- in any of the cohorts from a given start date (Jan 1, 2017, by default),
--- but are listed in the #fource_misc table.
--- The OtherMISCAdm cohort includes MISC patients who had an inpatient visit,
--- and OtherMISCNotAdm has MISC patients who had no inpatient visit.
+-- This code creates a cohort capturing all patients in the #fource_misc table.
 --
 -- Instructions: Search the 4CE_PhaseX.2_Files_*.sql script for the comment
 -- "Add additional custom cohorts here". Right below it insert the contents of
--- this script. To use a different start date, replace "1/1/2017" with the new
--- start date, and replace "1/1/2016" with one year prior to that start date.
--- The script will select all patients who have an encounter on or after the
--- "1/1/2017" date (or whatever you replace it with). It goes back to the
--- "1/1/2016" date to find admissions, ICU encounters, or death dates one year
--- prior to the index date (day 0). If you use a custom database schema, replace
--- "dbo." with the schema name. If you use multiple fact table, adjust the SQL
--- queries as needed.
+-- this script. If you use a custom database schema, replace "dbo." with the 
+-- schema name. If you use multiple fact table, adjust the SQL queries as
+-- needed. The first two lines of code delete all existing cohorts and patients.
+-- The output of the script, therefore, will just be the one MISC cohort.
+-- Comment out those lines to leave the existing cohorts and make a MISC cohort
+-- containing only the MISC patients who are not in other cohorts.
 --------------------------------------------------------------------------------
 
--- Add admissions for other patients
+-- *** Delete all existing cohorts and patients ***
+delete from #fource_cohort_config
+delete from #fource_cohort_patients
+
+-- Get admissions for MISC patients
 insert into #fource_admissions
 	select *
 	from (
@@ -46,7 +44,8 @@ insert into #fource_admissions
 	where (admission_date >= '1/1/2016') and (discharge_date >= admission_date)
 		and patient_num not in (select patient_num from #fource_cohort_patients)
 		and patient_num in (select patient_num from #fource_misc)
--- Add ICU dates for other patients
+
+-- Get ICU dates for MISC patients
 insert into #fource_icu
 	select *
 	from (
@@ -70,7 +69,8 @@ insert into #fource_icu
 	where (start_date >= '1/1/2016') and (end_date >= start_date)
 		and patient_num not in (select patient_num from #fource_cohort_patients)
 		and patient_num in (select patient_num from #fource_misc)
--- Add death dates for other patients
+
+-- Add death dates for MISC patients
 insert into #fource_death
 	select patient_num, isnull(death_date,'1/1/1900') 
 	from dbo.patient_dimension
@@ -78,32 +78,22 @@ insert into #fource_death
 		and patient_num not in (select patient_num from #fource_cohort_patients)
 		and death_date >= '1/1/2016'
 		and patient_num in (select patient_num from #fource_misc)
--- Define the cohorts
+
+-- Define the cohort
 ;with t as (
 	select '1/1/2017' start_date, isnull(source_data_updated_date,GetDate()) end_date
 	from #fource_config
 )
 insert into #fource_cohort_config
-	select 'OtherMISCAdm', 1, 1, end_date, start_date, end_date from t
-	union all
-	select 'OtherMISCNotAdm', 1, 1, end_date, start_date, end_date from t
--- Get all MISC patients who were admitted
+	select 'MISC', 1, 1, end_date, start_date, end_date from t
+
+-- Get the MISC patients, using the misc_date as the index date
 insert into #fource_cohort_patients
-	select c.cohort, a.patient_num, min(a.admission_date), c.source_data_updated_date, 0, null, null
-	from #fource_admissions a
+	select c.cohort, m.patient_num, min(m.misc_date), c.source_data_updated_date, 0, null, null
+	from #fource_misc m
 		inner join #fource_cohort_config c
-			on a.admission_date >= c.earliest_adm_date and a.admission_date <= c.latest_adm_date
-	where c.cohort='OtherMISCAdm'
-		and a.patient_num not in (select patient_num from #fource_cohort_patients)
-		and a.patient_num in (select patient_num from #fource_misc)
-	group by c.cohort, a.patient_num, c.source_data_updated_date
--- Get all other MISC patients who have an encounter
-insert into #fource_cohort_patients
-	select c.cohort, v.patient_num, min(v.start_date), c.source_data_updated_date, 0, null, null
-	from dbo.visit_dimension v
-		inner join #fource_cohort_config c
-			on v.start_date >= c.earliest_adm_date and v.start_date <= c.latest_adm_date
-	where c.cohort='OtherMISCNotAdm'
-		and v.patient_num not in (select patient_num from #fource_cohort_patients)
-		and v.patient_num in (select patient_num from #fource_misc)
-	group by c.cohort, v.patient_num, c.source_data_updated_date
+			on m.misc_date >= c.earliest_adm_date and m.misc_date <= c.latest_adm_date
+	where c.cohort='MISC'
+		and m.patient_num not in (select patient_num from #fource_cohort_patients)
+	group by c.cohort, m.patient_num, c.source_data_updated_date
+
